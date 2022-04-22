15bc2d0a205ecd6ac0b9c3552282028b72087f91
Muhammad Fiqri Dwi Nugraha <91336749+mfiqridn@users.noreply.github.com>
2022-04-22 15:13:27 +0700

    Upload Praktikum 4

---
+++ Praktikum_4/data_persegipanjang.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "class_PersegiPanjang.php";
+    $persegiPanjang1 = new PersegiPanjang( 12,4 );
+    $persegiPanjang2 = new PersegiPanjang( 16, 8 );
+
+    echo "<br/>Luas Persegi Panjang I ".$persegiPanjang1->getLuas();
+    echo "<br>Luas Persegi Panjang II ".$persegiPanjang2->getLuas();
+    
+    echo "<br>Keliling Persegi Panjang I ".$persegiPanjang1->getKeliling();
+    echo "<br>Keliling Persegi Panjang II ".$persegiPanjang2->getKeliling();
+?>
