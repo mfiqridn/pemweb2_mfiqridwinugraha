dd93f7601c79ab0962a38daf026440bc5ebabe96
Muhammad Fiqri Dwi Nugraha <91336749+mfiqridn@users.noreply.github.com>
2022-06-27 02:43:16 +0700

    Upload Praktikum 7

---
+++ Praktikum_7/application/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+class Home extends CI_Controller{
+	public function index(){
+
+		$data=["judul"=>"Praktikum 7"];
+		$this->load->view("layouts/header", $data);
+		$this->load->view('layouts/navbar');
+		$this->load->view("home/index");
+		$this->load->view("layouts/footer");
+	}
+}
+?>
