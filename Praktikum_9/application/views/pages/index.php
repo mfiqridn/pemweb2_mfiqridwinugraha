c7e3a266b16da5c3a74a2bb8f8ba894068453b27
Muhammad Fiqri Dwi Nugraha <91336749+mfiqridn@users.noreply.github.com>
2022-07-05 17:16:23 +0700

    Upload Praktikum 9

---
+++ Praktikum_9/application/views/pages/index.php
@@ -0,0 +1,11 @@
+<div class="container mt-4">
+    <div class="row ">
+        <div class="col">
+            <h2 class="display-6">Praktikum 9 - Login + CRUD</h2>
+            <hr />
+            <p class="text-center m-0">Nama: 0110221026</p>
+            <p class="text-center m-0">NIM: Muhammad Fiqri Dwi Nugraha</p>
+            <p class="text-center m-0">Rombel: TI16</p>
+        </div>
+    </div>
+</div>
